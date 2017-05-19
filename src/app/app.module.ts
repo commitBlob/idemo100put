de43dd417a36b365efc472cd4feb29a03f04a5ec
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-05-19 14:31:44 +0100

    Updated Angular created by cli and added Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedMaterialModule } from './sharedMaterialModule';
+
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedMaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/sharedMaterialModule.ts
@@ -0,0 +1,8 @@
+import { MaterialModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [MaterialModule],
+  exports: [ MaterialModule],
+})
+export class SharedMaterialModule { }
