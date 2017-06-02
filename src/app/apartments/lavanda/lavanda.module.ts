57cb5835e0ba6a9d8e43c917f5a7d573c2bcfc01
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-06-02 14:23:44 +0100

    Rollback, apartment specific navigation builder not working

--- src/app/apartments/lavanda/lavanda.module.ts
+++ src/app/apartments/lavanda/lavanda.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 // App specific
-import { ApartmentNavBuilderComponent } from '../../shared/navigation/apartment-selected-nav/selected-nav-builder.component';
 import { LavandaComponent } from './lavanda.component';
 import { LavandaFacilitiesComponent } from './lavanda-facilities/lavanda-facilities.component';
 import { LavandaPricelistComponent } from './lavanda-pricelist/lavanda-pricelist.component';
@@ -16,7 +15,6 @@ import { LavandaRoutes } from './lavanda.routes';
     RouterModule.forChild(LavandaRoutes)
   ],
   declarations: [
-    ApartmentNavBuilderComponent,
     LavandaComponent,
     LavandaPricelistComponent,
     LavandaFacilitiesComponent,
