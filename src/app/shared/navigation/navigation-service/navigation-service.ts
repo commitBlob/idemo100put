79b1ac2f97ee3fc9c3d608cbbef766a2aebc4586
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-06-02 16:25:38 +0100

    Need to replace promises with observables

--- src/app/shared/navigation/navigation-service/navigation-service.ts
+++ src/app/shared/navigation/navigation-service/navigation-service.ts
@@ -1,13 +1,20 @@
 // Core
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 // App specific
 import { NavLinks } from '../navigation-links.interface';
 import { Navigation, ApartmentSelectorNav, ApartmentsNav } from '../navigation-list';
 
+// TODO: replace promises with observables
+
 @Injectable()
 export class NavigationService {
+
+  // getNavigation(): Observable<NavLinks[]> {
+  //   return;
+  // }
 
   getNavigation(): Promise<NavLinks[]> {
     return Promise.resolve(Navigation);
