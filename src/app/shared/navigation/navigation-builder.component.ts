079c0e39f5d2a78e4abac7f963b6c0e4a185ccda
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-06-05 14:23:17 +0100

    WIP observables

--- src/app/shared/navigation/navigation-builder.component.ts
+++ src/app/shared/navigation/navigation-builder.component.ts
@@ -1,5 +1,6 @@
 // Core
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 // App specific
 import { NavLinks } from './navigation-links.interface';
@@ -11,10 +12,18 @@ import { NavigationService } from './navigation-service/navigation-service';
   styleUrls: ['navigation-builder.component.scss'],
   providers: [ NavigationService ]
 })
-export class NavigationBuilderComponent implements OnInit {
+export class NavigationBuilderComponent implements OnInit, OnDestroy {
+  mainNavigation: NavLinks[];
+  subscription: Subscription;
   public nav: NavLinks[];
   public apLinks: NavLinks[];
+
   constructor(private _navigationService: NavigationService) {
+  }
+
+  getMainNavigation() {
+    this.subscription = this._navigationService.getMainNavigation().subscribe(mainNavigation => { this.mainNavigation = mainNavigation; });
+    console.log('data?', this.subscription);
   }
 
   getNavigation() {
@@ -25,8 +34,16 @@ export class NavigationBuilderComponent implements OnInit {
     this._navigationService.getApartmentsList().then(( apLinks: NavLinks[]) => this.apLinks = apLinks)
   }
 
+  /* observable */
+
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }
+
   ngOnInit() {
     this.getNavigation();
     this.getApartmentsList();
+    this.getMainNavigation();
   }
 }
--- src/app/shared/navigation/navigation-service/navigation-service.ts
+++ src/app/shared/navigation/navigation-service/navigation-service.ts
@@ -1,6 +1,7 @@
 // Core
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/Rx';
 
 // App specific
@@ -11,10 +12,23 @@ import { Navigation, ApartmentSelectorNav, ApartmentsNav } from '../navigation-l
 
 @Injectable()
 export class NavigationService {
+  private _mainNavigation = new Subject<NavLinks[]>();
 
-  // getNavigation(): Observable<NavLinks[]> {
-  //   return;
-  // }
+
+  /* Ne radi */
+  /**
+   *  __responseData: Observable<any> = new Observable<any>();
+
+   constructor(private _http: Http) {
+    this.__responseData = this._http.get("./data.json")
+                            .map(res => res.json());
+  }
+   */
+
+  getMainNavigation(): Observable<NavLinks[]> {
+    console.log('sto sam', this._mainNavigation);
+    return this._mainNavigation.asObservable();
+  }
 
   getNavigation(): Promise<NavLinks[]> {
     return Promise.resolve(Navigation);
