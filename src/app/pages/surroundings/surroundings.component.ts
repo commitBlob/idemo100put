28b805863c780c05036faff282d43dac09c66ec9
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-07-27 14:41:36 +0100

    Surroiundings content

--- src/app/pages/surroundings/surroundings.component.ts
+++ src/app/pages/surroundings/surroundings.component.ts
@@ -2,14 +2,44 @@
 import { Component, OnInit } from '@angular/core';
 
 // App specific
+import { LanguagesService } from '../../shared/languages/languages.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ContentService } from '../../shared/content-service/content.service';
+
+// Models
+import { Content } from '../../shared/content-service/content.interface';
 
 @Component({
   templateUrl: './surroundings.component.html',
 })
 export class SurroundingsComponent implements OnInit {
-  constructor() { }
+
+  public surroundingsContent: Content[] = null;
+  public langSubscription: Subscription;
+  public language: String;
+
+  constructor(
+    private _languageService: LanguagesService,
+    private _contentService: ContentService
+  ) {
+    this.langSubscription = _languageService.subjectSourceAnnounced$.subscribe(
+      (value) => {
+        this.language = value;
+        this.getContent(this.language);
+      }
+    );
+  }
 
   public ngOnInit() {
+    this._languageService.getLanguage();
+    this.getContent(this.language);
+  }
 
+  public getContent(language) {
+    this._contentService.getSurroundingsContent(language).subscribe(
+      (content) => {
+        this.surroundingsContent = <Content[]>content;
+      }
+    );
   }
 }
