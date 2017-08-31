9143ef2d38d7d36cc6f34fed14274629df1aca9d
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-08-31 15:08:54 +0100

    Leftover

--- src/app/pages/apartments/apartment-details/nearby-places.interface.ts
+++ src/app/pages/apartments/apartment-details/nearby-places.interface.ts
@@ -3,6 +3,7 @@ export interface NearbyPlaces {
   // need this in order to filter by apartment
   apartmentName: string;
   placeName: string;
+  //  Croatian location name, would rather have extra field over another backend call
   imeLokacije: string;
   distance: number;
   icon: string;
