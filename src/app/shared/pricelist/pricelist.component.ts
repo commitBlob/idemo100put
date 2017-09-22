9bdd797a3f8abdd0e1c1a217e62dcd2be2ba4493
Maro Radovic <Maro.Radovic@smithsnews.co.uk>
2017-09-22 14:21:37 +0100

    removed xml2js as I use it on backend

--- src/app/shared/pricelist/pricelist.component.ts
+++ src/app/shared/pricelist/pricelist.component.ts
@@ -2,11 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
 
 // App specific
 import { PricelistService } from './pricelist.service';
-import * as xml2js from 'xml2js'
 
 // Models
 import { PricelistModel } from './pricelist.interface';
@@ -53,39 +51,31 @@ export class PricelistComponent implements OnInit {
     }
   }
 
-  // TODO: remove xml2js
-
   public doCalculation() {
     const tempCurrencyName = [];
     const tempCurrencyCourse = [];
     this._pricelistService.getCourseList().subscribe(
       (data) => {
-        console.log(data.body['gesmes:Envelope'].Cube[0].Cube[0].Cube);
-        // xml2js.parseString(data, (err, result) => {
-        //   // strip json
           data.body['gesmes:Envelope'].Cube[0].Cube[0].Cube.forEach((value: any) => {
             tempCurrencyName.push(value['$'].currency);
             tempCurrencyCourse.push(value['$'].rate);
           });
-          console.log(tempCurrencyName);
-        console.log(tempCurrencyCourse);
-        // });
       },
       (error) => {
         this.handleError(error);
       },
       () => {
-        // tempCurrencyName.forEach((value: any, key: any) => {
-        //   this.parsedCourseList[value] = tempCurrencyCourse[key];
-        // });
-        // this.parsedCourseList['EUR'] = '1';
-        // this.currencyList.forEach((value: any) => {
-        //   const tempValue = value;
-        //   if (this.parsedCourseList[tempValue]) {
-        //     this.courseList[tempValue] = this.parsedCourseList[tempValue]
-        //   }
-        // });
-        // console.log(this.courseList);
+        tempCurrencyName.forEach((value: any, key: any) => {
+          this.parsedCourseList[value] = tempCurrencyCourse[key];
+        });
+        this.parsedCourseList['EUR'] = '1';
+        this.currencyList.forEach((value: any) => {
+          const tempValue = value;
+          if (this.parsedCourseList[tempValue]) {
+            this.courseList[tempValue] = this.parsedCourseList[tempValue]
+          }
+        });
+        console.log(this.courseList);
       });
   }
 
