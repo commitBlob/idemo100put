f5574a09d3a7935ea841a96564b2737d425cb441
Maro <Maro.Radovic@smithsnews.co.uk>
2018-02-01 11:25:18 +0000

    feat(Responsive Service): Set Mode on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,6 +34,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.getNavigation();
+    // Get the responsive responsiveMode on load.
+    this.responsiveService.set();
   }
 
   getNavigation() {
