48659f2df4e2b9dbc6b35eae6f16a3d29f4c0963
Maro <Maro.Radovic@smithsnews.co.uk>
2018-02-01 11:48:39 +0000

    feat(Google charts): Bar Chart

--- src/app/shared/google-charts/google-column-chart/google-column.component.ts
+++ src/app/shared/google-charts/google-column-chart/google-column.component.ts
@@ -20,24 +20,38 @@ export class GoogleColumnComponent implements OnInit {
 
   drawChart() {
 
-    const data = new google.visualization.DataTable();
+    // const data = new google.visualization.DataTable();
+    // data.addColumn('string', 'Month of the Year');
+    // data.addColumn('number', 'Visitors Level');
+    // data.addRows([
+    //   [{v: 'Jan'}, 2],
+    //   [{v: 'Feb'}, 1],
+    //   [{v: 'Mar'}, 1],
+    //   [{v: 'Apr'}, 2],
+    //   [{v: 'May'}, 3],
+    //   [{v: 'Jun'}, 4],
+    //   [{v: 'Jul'}, 4],
+    //   [{v: 'Aug'}, 4],
+    //   [{v: 'Sep'}, 3],
+    //   [{v: 'Oct'}, 2],
+    //   [{v: 'Nov'}, 1],
+    //   [{v: 'Dec'}, 1],
+    // ]);
 
-    data.addColumn('string', 'Month of the Year');
-    data.addColumn('number', 'Visitors Level');
-
-    data.addRows([
-      [{v: 'Jan'}, 2],
-      [{v: 'Feb'}, 1],
-      [{v: 'Mar'}, 1],
-      [{v: 'Apr'}, 2],
-      [{v: 'May'}, 3],
-      [{v: 'Jun'}, 4],
-      [{v: 'Jul'}, 4],
-      [{v: 'Aug'}, 4],
-      [{v: 'Sep'}, 3],
-      [{v: 'Oct'}, 2],
-      [{v: 'Nov'}, 1],
-      [{v: 'Dec'}, 1],
+    const data = new google.visualization.arrayToDataTable([
+      ['Month of the Year', 'Visitors Level', { role: 'style' }],
+        [{v: 'Jan'}, 2, 'green'],
+        [{v: 'Feb'}, 1, 'olive'],
+        [{v: 'Mar'}, 1, 'olive'],
+        [{v: 'Apr'}, 2, 'green'],
+        [{v: 'May'}, 3, 'yellow'],
+        [{v: 'Jun'}, 4, 'red'],
+        [{v: 'Jul'}, 4, 'red'],
+        [{v: 'Aug'}, 4, 'red'],
+        [{v: 'Sep'}, 3, 'yellow'],
+        [{v: 'Oct'}, 2, 'green'],
+        [{v: 'Nov'}, 1, 'olive'],
+        [{v: 'Dec'}, 1, 'olive'],
     ]);
 
     const options = {
