5009164db9dbd0eac458b4addad8bb40e88a19ae
commitBlob <maro.radovic12@gmail.com>
2021-07-07 13:52:08 +0100

    refactor: Material Modules Imports

--- src/app/materialModule.ts
+++ src/app/materialModule.ts
@@ -1,10 +1,15 @@
 // Core
-import {
-  MatTabsModule, MatCheckboxModule, MatInputModule, MatSelectModule,
-  MatButtonModule, MatDialogModule, MatTooltipModule, MatDatepickerModule, DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS
-} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule} from '@angular/material/dialog';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
 const modules = [
   MatTabsModule,
